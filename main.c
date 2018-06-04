897d94b56ceccde4120eab9119f7da323dbdf6cf
nozaryo <nozaryo0730@gmail.com>
2018-06-05 00:47:28 +0900

    make sabu fallder

--- main.c
+++ main.c
@@ -7,7 +7,11 @@
 #include <avr/interrupt.h>
 
 int timer = 0;
-int steper1[3]
+int stepper[3][3][2]={{{0,0},{0,0},{0,0}},//{steps,speed}
+									    {{0,0},{0,0},{0,0}},
+										  {{0,0},{0,0},{0,0}}};
+										 //if steps == 0 && speed == 0 stop
+										 //if steps == 0 && speed != 0 infinty spin
 
 ISR(TIMER2_COMPA_vect){//1ms毎に割り込み
 	timer ++;
@@ -17,6 +21,8 @@ ISR(TIMER2_COMPA_vect){//1ms毎に割り込み
 	}
 }
 int init();
+int set_stepper_step(int x,int y,int num);
+int set_stepper_speed(int x,int y,int num);
 
 int main(void){
 
@@ -35,4 +41,22 @@ int init(){
 	TIMSK2 = 0b00000010;
 	sei();
 	return 0;
+}
+
+int set_stepper_step(int x,int y,int num){
+	if(x<3 && y<3){
+		stepper[x][y][0] = num;
+		return 1;
+	}else{
+		return 0;
+	}
+}
+
+int set_stepper_speed(int x,int y,int num){
+	if(x<3 && y<3){
+		stepper[x][y][1] = num;
+		return 1;
+	}else{
+		return 0;
+	}
 }
